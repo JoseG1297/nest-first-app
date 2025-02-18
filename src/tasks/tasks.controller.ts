24f4e576fc1f369350744151c0a2dbc81dc0b2ae
JoseG1297 <mordo1309@gmail.com>
2025-02-18 16:49:38 -0600

    inyectar clase task en controlador

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     
+    constructor(private tasksService: TasksService) {}
+
+    @Get()
+    getAllTasks() {
+        return this.tasksService.getAllTasks();
+    }
 }
