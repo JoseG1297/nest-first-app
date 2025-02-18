65c235c199518d7709fbae59ba3b6bf8ad249d6a
JoseG1297 <mordo1309@gmail.com>
2025-02-18 16:48:44 -0600

    creando servicios

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,37 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: this.tasks.length + 1,
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+        this.tasks.push(task);
+        return task;
+    }
+
+    getTaskById(id: number): Task {
+        return this.tasks.find(task => task.id === id) || null;
+    }
+
+    deleteTask(id: number): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+
+    updateTaskStatus(id: number, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
+}
